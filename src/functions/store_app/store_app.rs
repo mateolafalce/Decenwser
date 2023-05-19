d0c93535ffbcd8a222db742ecc871637296673e4
Mateo Lafalce <mateolafalce3@gmail.com>
2023-05-20 05:20:06 +1100

    store_app.rs comments

--- src/functions/store_app/store_app.rs
+++ src/functions/store_app/store_app.rs
@@ -1,42 +1,47 @@
 /*
-    -Its saves the rendered app from the solana blockchain pdas locally in the user computer.
+    This function saves the rendered app from the Solana blockchain PDAs locally on the user's computer.
 */
 
 use anyhow::{
-    Result, 
-    Ok
+    Result,
+    Context,
 };
 use std::{
-    io::Write, fs::{
-        read,
-        OpenOptions, 
-        File
-    }
+    fs::{self, OpenOptions, File},
+    io::Write,
 };
 use crate::functions::get_page::get_domain;
 
 pub fn store_app() -> Result<()> {
-    let iter: [String;2] = [
-        ("apps/".to_owned() + &get_domain().unwrap() + ".html").to_string(),
-        ("apps/".to_owned() + &get_domain().unwrap() + ".js").to_string()
+    // Create an array of file paths for the app HTML and JavaScript files
+    let iter: [String; 2] = [
+        format!("apps/{}.html", get_domain()?),
+        format!("apps/{}.js", get_domain()?),
     ];
-    let to_read: [String;2] = [
-        "templates/web.html.hbs".to_string(), 
-        "public/js.js".to_string()
+    // Create an array of file paths to read from
+    let to_read: [String; 2] = [
+        "templates/web.html.hbs".to_string(),
+        "public/js.js".to_string(),
     ];
+    // Iterate over the file paths and process each file
     for i in 0..2 {
-        let file: File = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(iter[i].to_owned()).expect("Error");
-        let mut write_file: File = file.try_clone().expect("Error");
-        write_file.write(&read(to_read[i].to_owned()).unwrap()).unwrap(); 
+        // Open the file for writing, creating it if it doesn't exist, and truncate its content
+        let file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(iter[i].to_owned())
+            .with_context(|| format!("Failed to open file: {}", iter[i]))?;
+        let mut write_file = file.try_clone().with_context(|| format!("Failed to clone file: {}", iter[i]))?;
+        // Read the content from the file to be copied
+        let content = fs::read(to_read[i].to_owned()).with_context(|| format!("Failed to read file: {}", to_read[i]))?;
+        // Write the content to the destination file
+        write_file.write_all(&content).with_context(|| format!("Failed to write to file: {}", iter[i]))?;
     }
     Ok(())
 }
 
 #[post("/")]
 pub fn index() {
-    store_app().unwrap()
-}       
+    store_app().expect("Failed to store app");
+}
