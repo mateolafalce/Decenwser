03e42738d594e1e7ffd225d2f54c32ada1fbc2de
Mateo Lafalce <mateolafalce3@gmail.com>
2023-05-21 00:59:47 +1100

    encode.rs

--- src/functions/send_app/encode.rs
+++ src/functions/send_app/encode.rs
@@ -1,19 +1,19 @@
 /*
-    -The input from the html or js file is received and passed to utf-8
+    - The input from the HTML or JS file is received and passed to UTF-8.
 */
 
 use rocket::serde::{
-    Deserialize, 
-    Serialize, 
+    Deserialize,
+    Serialize,
     json::{
         Json,
-        from_str, 
+        from_str,
         to_string
     }
 };
 use std::{
     fs::{
-        write, 
+        write,
         read_to_string
     }
 };
@@ -35,31 +35,37 @@ pub struct Price {
 }
 
 pub fn encode(input: String, html_js: String) -> Result<Price> {
+    // Split the input into chunks of 900 bytes and convert them to a vector of byte vectors.
     let encode: Vec<Vec<u8>> = input.as_bytes()
         .chunks(900)
         .map(|chunk| chunk.to_vec())
         .collect();
     if html_js == "html" {
+        // Read the configuration from the encode_html.json file.
         let mut config: Encode = from_str(&read_to_string("src/functions/encode_output/encode_html.json").unwrap()).unwrap();
         config.content = encode;
+        // Write the updated configuration back to the encode_html.json file.
         write("src/functions/encode_output/encode_html.json", to_string(&config).unwrap()).unwrap();
         let price: Price = Price {
+            // Calculate the price based on the length of the content.
             price: (config.content.len() * 6269000) as u64,
             html_js: html_js
         };
         Ok(price)
-    } else  {
+    } else {
+        // Read the configuration from the encode_js.json file.
         let mut config: Encode = from_str(&read_to_string("src/functions/encode_output/encode_js.json").unwrap()).unwrap();
         config.content = encode;
+        // Write the updated configuration back to the encode_js.json file.
         write("src/functions/encode_output/encode_js.json", to_string(&config).unwrap()).unwrap();
         let price: Price = Price {
+            // Calculate the price based on the length of the content.
             price: (config.content.len() * 6269000) as u64,
             html_js: html_js
         };
         Ok(price)
     }
 }
-
 
 #[post("/", data = "<data>")]
 pub fn index(data: Json<EncodeData>) -> Json<Price> {
